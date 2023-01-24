79d87697ea597f0698fabd1e2344637f9fa9780d
abhijitbhosale <abhijitbhosale96@gmail.com>
2023-01-24 13:00:03 +0530

    Update GithubActionGkeApplication.java

--- src/main/java/com/githubaction/code/githubactiongke/GithubActionGkeApplication.java
+++ src/main/java/com/githubaction/code/githubactiongke/GithubActionGkeApplication.java
@@ -1,14 +1,20 @@
 package com.githubaction.code.githubactiongke;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GithubActionGkeApplication {
+public class GithubActionGkeApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(GithubActionGkeApplication.class, args);
 		System.out.println("####################  GithubActionGkeApplication Main Called ##################");
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("####################  GithubActionGkeApplication run Called ##################");
+		System.exit(0);
+	}
 }
